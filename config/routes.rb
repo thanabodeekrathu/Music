52084f2a09b252c55b3e1c49a987678c923678ac
thanabodeekrathu <thanabodee.thkt@gmail.com>
2018-06-21 14:47:46 +0700

    add training file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :beta
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  get '/user/get_demo' , to: 'users#get_demo', as: :demo_user
+  get '/user/get_params/:v1', to: 'users#get_demo2', as: :demo_user2
 end
